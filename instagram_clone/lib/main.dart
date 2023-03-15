f292caaa145190031f01c48f57a8477cbcc23b97
sinif1-18 <sinif1-18@sinif1-18-iMac.local>
2023-03-15 16:55:17 +0300

    story kodları yazıldı

---
+++ instagram_clone/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/components/timeline_appbar.dart';
+import 'package:instagram_clone/instagram_stories.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: TimelineAppbar(),
+      body: StoriesInstagram(),
+    );
+  }
+}
