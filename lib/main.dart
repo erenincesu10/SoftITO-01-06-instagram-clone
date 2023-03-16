0583759d4c6754b079b4c8917cc33023a4ee4ce5
Umut Kaan Kartaloğlu <umutkaankartaloglu9@gmail.com>
2023-03-16 10:04:25 +0300

    story
    güncellemesi yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/components/bottomNavBarPage.dart';
+import 'package:instagram_clone/components/instagram_stories.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: bottomNavBarPage(),
+      home: StoriesInstagram(),
     );
   }
 }
