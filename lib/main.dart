f1e2e13834dc09f645fc1c02aa264327d1b5b306
Umut Kaan Kartaloğlu <umutkaankartaloglu9@gmail.com>
2023-03-16 10:09:05 +0300

    update
    yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,22 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StoriesInstagram(),
+      bottomNavigationBar: bottomNavBarPage(),
+    );
+  }
+}
