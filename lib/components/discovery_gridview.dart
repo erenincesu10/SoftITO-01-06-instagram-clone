37c54a78b8872e8bc4eb2614eb145b756774c5b1
Umut Kaan Kartaloğlu <umutkaankartaloglu9@gmail.com>
2023-03-17 09:21:40 +0300

    messagepagestatus

--- lib/components/discovery_gridview.dart
+++ lib/components/discovery_gridview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_clone/components/discovery_gridview.dart';
 import 'bottomNavBarPage.dart';
 
