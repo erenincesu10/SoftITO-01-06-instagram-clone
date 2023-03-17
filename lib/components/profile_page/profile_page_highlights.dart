5b5710b85488789f454596c02362a051784038df
Eren İncesu <erenincesu.1@hotmail.com>
2023-03-17 11:53:19 +0300

    updates done

--- lib/components/profile_page/profile_page_highlights.dart
+++ lib/components/profile_page/profile_page_highlights.dart
@@ -24,6 +24,7 @@ class _ProfilePageHighlightsState extends State<ProfilePageHighlights> {
                 HighlightUser(),
                 HighlightUser(),
                 HighlightUser(),
+                HighlightUser(),
                 HighlightAdd(),
               ],
             ),
